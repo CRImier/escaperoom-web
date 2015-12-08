cf9d2cd6a659c3237cd1fe3883785710980d3d62
user <user@debian>
2015-12-08 19:05:19 +0200

    security near done (visuals)

--- escaperoom-guardroom/script.js
+++ escaperoom-guardroom/script.js
@@ -1,15 +1,27 @@
+function resetLogin() {
+    "use strict";
+    document.getElementById('access').style.display = 'none';
+    document.getElementById('info_block').style.outline = '3px solid #0F0';
+    document.getElementById('password_block').style.display = 'block';
+}
+
 function checkPassword() {
     "use strict";
     var msgTimer;
     var password = document.getElementById('password_field').value;
 
     if (password === "correcthorse") {
-        window.alert('omelette du fromage');
+        document.getElementById('password_block').style.display = 'none';
+        document.getElementById('info_block').style.left = "25%";
+        document.getElementById('info_block').style.top = "5%";
+        document.getElementById('info_block').style.width = "50%";
+        document.getElementById('info_block').style.height = "70%";
+        document.getElementById('security_console').style.display = 'block';
     } else {
         document.getElementById('access').style.display = 'block';
-        document.getElementById('info_block').style.outline = '3px solid #F00'
+        document.getElementById('info_block').style.outline = '3px solid #F00';
         document.getElementById('password_block').style.display = 'none';
-        msgTimer = setTimeout(resetLogin, 3000);
+        msgTimer = setTimeout(resetLogin, 2500);
     }
 }
 
@@ -18,10 +30,4 @@ function checkSubmit(e) {
     if (e && e.keyCode === 13) {
         checkPassword();
     }
-}
-
-function resetLogin() {
-    document.getElementById('access').style.display = 'none';
-    document.getElementById('info_block').style.outline = '3px solid #0F0';
-    document.getElementById('password_block').style.display = 'block';
 }
